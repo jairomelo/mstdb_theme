ac008faa937d5f5b89c2bc2bb07387b0a42433d3
jairomelo <jairoantoniomelo@gmail.com>
2024-09-06 23:55:17 -0600

    lugares search cards

--- src/routes/(app)/Search/+page.svelte
+++ src/routes/(app)/Search/+page.svelte
@@ -549,11 +549,29 @@
 					{#each $searchResultsStore.groupedResults.Lugares as lugar}
 						<div class="list-group-item list-group-item-action">
 							<div class="d-flex w-100 justify-content-between">
-								<h5 class="mb-1">{lugar.lugar}</h5>
-								<small>{lugar.tipo_lugar || 'N/A'}</small>
+								<a href="/Detail/lugares/{lugar.lugar_id}">
+								<h5 class="mb-1">{lugar.nombre_lugar} {#if lugar.tipo_lugar}({lugar.tipo_lugar}){/if}</h5>
+							</a>
 							</div>
-							<p class="mb-1">Región: {lugar.region || 'Desconocida'}</p>
-							<small>Coordenadas: {lugar.coordenadas || 'N/A'}</small>
+							<p class="mb-1">Personas relacionadas:</p>
+							{#if lugar.personas_relacionadas.length > 0}
+								{#each lugar.personas_relacionadas.slice(0, 5) as relaciones, index}
+									{#each relaciones.personas as per}
+									<a class="{per.polymorphic_ctype?.includes('persona esclavizada') ? 'text-primary' : 'text-secondary'}" 
+									href="/Detail/{per.polymorphic_ctype?.includes('persona esclavizada') ? 'personaEsclavizada' : 'personaNoEsclavizada'}/{per.persona_id}">
+											{per.nombre_normalizado}
+										</a>
+										{#if index < 4 && index < lugar.personas_relacionadas.length - 1} | {/if}
+									{/each}
+								{/each}
+								{#if lugar.personas_relacionadas.length > 5}
+									<a href="/Detail/lugares/{lugar.lugar_id}" class="ms-2" use:tooltip={{ title: 'Ver todas las personas relacionadas', trigger: 'hover' }}>
+										<i class="bi bi-plus-circle"></i>
+									</a>
+								{/if}
+							{:else}
+								<span>No hay personas relacionadas</span>
+							{/if}
 						</div>
 					{/each}
 				</div>
