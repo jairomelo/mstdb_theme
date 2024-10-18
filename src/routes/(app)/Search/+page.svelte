89d11d64928996273870c48a1fcb1b0724b80827
jairomelo <jairoantoniomelo@gmail.com>
2024-10-18 09:05:20 -0600

    documento cards refactor

--- src/routes/(app)/Search/+page.svelte
+++ src/routes/(app)/Search/+page.svelte
@@ -10,6 +10,10 @@
 	// import config files
 	import { filtersConfig } from '$conf/filters.js';
 
+
+	// import components
+	import DocumentCard from './DocumentCard.svelte';
+
 	// define variables
 	export let data;
 	let { searchQuery, filter } = data;
@@ -232,40 +236,7 @@
 				<h3 class="mt-4 mb-3"><i class="bi bi-file-text me-2"></i>Documentos</h3>
 				<div class="list-group mb-4">
 					{#each $searchResultsStore.groupedResults.Documentos as doc}
-						<div class="list-group-item list-group-item-action">
-							<div class="d-flex w-100 justify-content-between">
-								<a href="/Detail/documentos/{doc.documento_id}">
-								<h5 class="mb-1">{doc.titulo}</h5>
-								</a>
-								<small
-									>{doc.fecha_inicial_raw || 'N/A'}{doc.fecha_final_raw &&
-									doc.fecha_final_raw !== doc.fecha_inicial_raw
-										? ` - ${doc.fecha_final_raw}`
-										: ''}</small
-								>
-							</div>
-							<p class="mb-1">
-								{doc.archivo.nombre_abreviado || 'No archivo'},
-								{#if doc.fondo}
-									{doc.fondo || 'Sin fondo'},{/if}
-								{#if doc.subfondo}
-									{doc.subfondo},{/if}
-								{#if doc.serie}
-									{doc.serie},{/if}
-								{#if doc.subserie}
-									{doc.subserie},{/if}
-								{#if doc.tipo_udc}
-									{doc.tipo_udc}.{/if}
-								{#if doc.unidad_documental_compuesta}
-									{doc.unidad_documental_compuesta}.{/if}
-								{#if doc.folio_final}
-									ff. {doc.folio_inicial}-{doc.folio_final}
-								{:else if doc.folio_inicial}
-									f. {doc.folio_inicial}
-								{/if}
-							</p>
-							<small>Identificador: {doc.documento_idno || 'N/A'}</small>
-						</div>
+						<DocumentCard {doc} />
 					{/each}
 				</div>
 			{/if}
---
+++ src/routes/(app)/Search/DocumentCard.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let doc;
+</script>
+
+<div class="list-group-item list-group-item-action">
+	<div class="d-flex w-100 justify-content-between">
+		<a href="/Detail/documentos/{doc.documento_id}">
+			<h5 class="mb-1">{doc.titulo}</h5>
+		</a>
+		<small>{doc.fecha_inicial_raw || 'N/A'}{doc.fecha_final_raw && doc.fecha_final_raw !== doc.fecha_inicial_raw ? ` - ${doc.fecha_final_raw}` : ''}</small>
+	</div>
+	<p class="mb-1">
+		{doc.archivo.nombre_abreviado || 'No archivo'}
+		{#if doc.fondo}{doc.fondo},{/if}
+		{#if doc.subfondo}{doc.subfondo},{/if}
+		{#if doc.serie}{doc.serie},{/if}
+		{#if doc.subserie}{doc.subserie},{/if}
+		{#if doc.tipo_udc}{doc.tipo_udc}.{/if}
+		{#if doc.unidad_documental_compuesta}{doc.unidad_documental_compuesta}.{/if}
+		{#if doc.folio_final}ff. {doc.folio_inicial}-{doc.folio_final}{:else if doc.folio_inicial}f. {doc.folio_inicial}{/if}
+	</p>
+	<small>Identificador: {doc.documento_idno || 'N/A'}</small>
+</div>
