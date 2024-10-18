49127c8dd35888045ba5f37c896fca448f427107
jairomelo <jairoantoniomelo@gmail.com>
2024-10-18 09:00:31 -0600

    template level configuration

---
+++ src/conf/filters.js
@@ -0,0 +1,16 @@
+// filters.js
+
+/**
+ * This file defines the configuration for filters used in the application.
+ * It exports an array of filter configurations, each containing a value, name, icon, and an active status.
+ * The filters are used to categorize and display different types of data in the application.
+ */
+
+export const filtersConfig = [
+	{ value: 'all', name: 'Todo', icon: 'bi-grid-3x3-gap', active: true },
+	{ value: 'documentos', name: 'Documentos', icon: 'bi-file-text', active: true },
+	{ value: 'personas_esclavizadas', name: 'Personas Esclavizadas', icon: 'bi-person', active: true },
+	{ value: 'personas_no_esclavizadas', name: 'Personas No Esclavizadas', icon: 'bi-person-check', active: true },
+	{ value: 'corporaciones', name: 'Corporaciones', icon: 'bi-building', active: false },
+	{ value: 'lugares', name: 'Lugares', icon: 'bi-geo-alt', active: true },
+];
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
 
 export default {
   kit: {
     adapter: adapter({
       out: 'build' // Output directory for the Node.js server
     }),
+    alias: {
+      $conf: path.resolve('src/conf'),
+    },
     // Add other necessary configurations
-  }
+  },
+  preprocess: vitePreprocess()
 };
