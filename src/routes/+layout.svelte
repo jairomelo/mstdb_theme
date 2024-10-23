f3a4c6429a6b69d179608e937d5c1c413040460b
jairomelo <jairoantoniomelo@gmail.com>
2024-10-22 20:49:42 -0700

    fix svelte # is not href valid attribute

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
             <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Browse
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
