7247834ec2df6d43c4fe764922596d32a950be9d
jairomelo <jairoantoniomelo@gmail.com>
2024-10-29 16:19:03 -0700

    icons mod

--- src/conf/filters.js
+++ src/conf/filters.js
@@ -2,8 +2,8 @@
 export const filtersConfig = [
 	{ value: 'all', name: 'Todo', icon: 'bi-grid-3x3-gap', active: true },
 	{ value: 'documento', name: 'Documentos', icon: 'bi-file-text', active: true },
-	{ value: 'personaesclavizada', name: 'Personas Esclavizadas', icon: 'bi-person', active: true },
-	{ value: 'personanoesclavizada', name: 'Personas No Esclavizadas', icon: 'bi-person-check', active: true },
+	{ value: 'personaesclavizada', name: 'Personas Esclavizadas', icon: 'bi-person-lock', active: true },
+	{ value: 'personanoesclavizada', name: 'Personas No Esclavizadas', icon: 'bi-person', active: true },
 	{ value: 'corporacion', name: 'Corporaciones', icon: 'bi-building', active: true },
 	{ value: 'lugar', name: 'Lugares', icon: 'bi-geo-alt', active: true },
   ];
