a08b4d41a8147f50a71d4f4a5a6c95f91304c79d
jairomelo <jairoantoniomelo@gmail.com>
2024-12-01 21:13:25 -0800

    basic style

--- src/routes/(app)/Browse/personasesclavizadas/+page.svelte
+++ src/routes/(app)/Browse/personasesclavizadas/+page.svelte
@@ -1,26 +1,29 @@
 <script>
     import { onMount } from 'svelte';
-
     import { personasescfull } from '$lib/api';
     
     let isClient = false;
 
-    onMount( async () => {
+    onMount(async () => {
         isClient = true;
 
         if (isClient) {
-
             const jQuery = (await import('jquery')).default;
-
             window.$ = window.jQuery = jQuery;
         
-            const { initDataTable, columns } = await import('$lib/datatable'); // if you want to add or replace columns, do it here
-
+            const { initDataTable, columns } = await import('$lib/datatable'); // Define or modify columns here
             initDataTable('dataTable', columns, personasescfull);
         }
     });
 </script>
 
-<div>
-    <table id="dataTable" class="table table-striped"></table>
+<div class="container mt-4">
+    <div class="card shadow">
+        <div class="card-header bg-primary text-white">
+            <h3 class="card-title mb-0">Personas Esclavizadas</h3>
+        </div>
+        <div class="card-body">
+            <table id="dataTable" class="table table-striped table-hover order-column"></table>
+        </div>
+    </div>
 </div>
