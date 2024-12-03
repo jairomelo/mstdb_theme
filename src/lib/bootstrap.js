857e374f420c5742724bd6f9b48d188cbb54360b
jairomelo <jairoantoniomelo@gmail.com>
2024-12-02 23:30:39 -0800

    Refactor Bootstrap component imports to use Promise.all for improved loading efficiency in bootstrap.js

--- src/lib/bootstrap.js
+++ src/lib/bootstrap.js
@@ -1,8 +1,9 @@
 import { browser } from '$app/environment';
 
-if (browser) {
-  import('bootstrap/js/dist/dropdown');
-  import('bootstrap/js/dist/collapse');
-  import('bootstrap/js/dist/tooltip');
-  // Add other Bootstrap components as needed
-}
+export const bootstrapReady = browser
+  ? Promise.all([
+      import('bootstrap/js/dist/dropdown'),
+      import('bootstrap/js/dist/collapse'),
+      import('bootstrap/js/dist/tooltip')
+    ])
+  : Promise.resolve();
