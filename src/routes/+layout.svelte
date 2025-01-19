acbafff551b6b372bd5b2cc2ae5267d0d13974df
jairomelo <jairoantoniomelo@gmail.com>
2025-01-19 11:04:30 -0800

    Remove  navigation elements from the homepage layout in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,46 +17,6 @@
 </script>
 
 {#if $page.url.pathname === '/'}
-	<nav class="navbar navbar-dark fixed-top bg-transparent">
-		<div class="container-fluid">
-			<button
-				class="navbar-toggler ms-2"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-				use:collapse
-			>
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item dropdown">
-						<a
-							class="nav-link dropdown-toggle"
-							id="navbarDropdown"
-							role="button"
-							data-bs-toggle="dropdown"
-							aria-expanded="false"
-							use:dropdown
-						>
-							Explorar la base de datos
-						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdown" data-bs-popper="none">
-							<li>
-								<a class="dropdown-item" href="/Browse/personasesclavizadas/"
-									>Personas Esclavizadas</a
-								>
-							</li>
-							<li><hr class="dropdown-divider" /></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
 	<slot />
 {:else}
 	<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
