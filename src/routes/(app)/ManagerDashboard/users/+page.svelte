f448c0ec7943cb5b528b635e3acdfce35ab1febb
jairomelo <jairoantoniomelo@gmail.com>
2025-05-18 11:30:58 -0700

    add user dashboard with authentication check

---
+++ src/routes/(app)/ManagerDashboard/users/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from "svelte";
+    import { whoami } from "$lib/api";
+
+    let user = null;
+    let error = null;
+
+    onMount(async () => {
+	try {
+		user = await whoami();
+	} catch (err) {
+		console.error(err);
+		error = "You are not logged in";
+	}
+});
+
+
+</script>
+
+
+{#if user}
+	<h1>Welcome to the Dashboard</h1>
+	<p>{#if user.is_staff}<i class="bi bi-person-square"></i>{/if} <strong>Username:</strong> {user.username}</p>
+	<p><strong>Email:</strong> {user.email}</p>
+{:else if error}
+	<p style="color: red;">{error}</p>
+{:else}
+	<p>Loading...</p>
+{/if}
