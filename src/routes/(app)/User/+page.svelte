a06ae78f3209a0b794a9edb92da45f9cb60284a2
jairomelo <jairoantoniomelo@gmail.com>
2026-05-16 22:03:14 -0700

    feat(profile): enhance profile link styling in user header

--- src/routes/(app)/User/+page.svelte
+++ src/routes/(app)/User/+page.svelte
@@ -115,7 +115,8 @@
     <div class="d-flex align-items-center justify-content-between mb-1">
         <h1 class="h3 mb-0">
             {#if user.is_staff}<i class="bi bi-person-badge me-2" aria-hidden="true"></i>{/if}
-            {user.username} <a href='/User/profile'>[perfil]</a>
+            {user.username} <a href='/User/profile' class="btn btn-link btn-sm ps-0 text-secondary" role="button"><i class="bi bi-person-fill-gear me-1" aria-hidden="true"></i>[perfil]</a>
+            
         </h1>
         <button class="btn btn-outline-secondary btn-sm" on:click={handleLogout}>
             <i class="bi bi-box-arrow-right me-1" aria-hidden="true"></i>Cerrar sesión
